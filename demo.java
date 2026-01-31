8aa6e8266f8afed139de9cbc52ad9cc03668a01e
cNavyasri2005 <cnavyareddynavya@gmail.com>
2026-01-31 14:12:32 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo{
+    public static void main(String[]args){
+    System.out.println("hello");
+    System.out.println("world");
+    System.out.print("world");
+    System.out.println("hello");
+
+}
+}
